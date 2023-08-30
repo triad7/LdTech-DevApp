2fd83aaaf9485ac3da849defc9bad7c33f9fd9e4
sambit801 <sambit.nayak2018@gift.edu.in>
2023-08-30 22:55:18 +0530

    Updated By Sambit801 :rocket:

--- src/components/Subpages/Content.js
+++ src/components/Subpages/Content.js
@@ -1,14 +1,23 @@
+
 import React from 'react';
 import "./Content.css";
-
+import backgroundVideo from '../../videos/video.mp4'; 
 
 const Content = () => {
   return (
-    <section id="content">
-      <div className="content-container">
-        <h2>Transfer Your Operations with DevOps</h2>
-        <p>Effortlessly automate your software development and delivery process with our cutting-edge technology.</p>
-        <button>Try Now</button>
+    <section id="content" className="video-bg">
+      <div className="video-container">
+        <video autoPlay loop muted>
+          <source src={backgroundVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="content-overlay">
+          <div className="content-container">
+            <h2>Transfer Your Operations with DevOps</h2>
+            <p>Effortlessly automate your software development and delivery process with our cutting-edge technology.</p>
+            <button>Try Now</button>
+          </div>
+        </div>
       </div>
     </section>
   );
